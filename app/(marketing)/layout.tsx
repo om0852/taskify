2a7e0d910ad00a1abe316c58105e03a3695c2377
om salunke <omsaunke343@gmail.com>
2024-08-13 09:44:04 +0530

    navbar and footer added

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/Footer";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full bg-slate-100">
       <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
-      {/* footer  */}
+<Footer/>
     </div>
   );
 };
