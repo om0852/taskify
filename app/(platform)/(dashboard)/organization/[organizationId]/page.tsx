098758806a6c5f3bcfe335a22d60010e3acdb901
om salunke <omsaunke343@gmail.com>
2024-08-14 20:41:28 +0530

    database connection created

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -3,10 +3,16 @@ import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 const Page = () => {
-  const { userId, orgId } = auth();
+  async function create(formData:FormData){
+    "use server"
+    const title = formData.get("title")
+    console.log("i am trigger")
+  }
   return (
     <div>
-      org
+      <form action={create}>
+        <input id="title" name="title" required placeholder="Enter a board title" className="border-black border p-1"/>
+      </form>
           </div>
   );
 };
