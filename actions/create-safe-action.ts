654186213d94bd06cd5008f0e4f716bad696dfb6
om salunke <omsaunke343@gmail.com>
2024-08-19 10:27:14 +0530

    safe action created

--- actions/create-safe-action.ts
+++ actions/create-safe-action.ts
@@ -13,5 +13,13 @@ export const createSafeAction = <TInput,TOuput>(
     schema:Schema<TInput>,
     handler:(validationData:TInput)=>Promise<ActionState<TInput,TOuput>>
 )=>{
-    return async(data:TInput):Promise<ActionState<TInput.TOuput>>
+    return async(data:TInput):Promise<ActionState<TInput,TOuput>>=>{
+        const validationResult = schema.safeParse(data)
+        if(!validationResult.success){
+            return {
+                fieldErrors:validationResult.error.flatten().fieldErrors as FieldError<TInput>
+            }
+        }
+        return handler(validationResult.data);
+    }
 }
