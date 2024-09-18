cc9acaa2b07438053f0fd3bbe7e99687418cc04c
om salunke <omsaunke343@gmail.com>
2024-09-18 23:39:24 +0530

    making submit button

--- components/form/form-submit.tsx
+++ components/form/form-submit.tsx
@@ -4,15 +4,41 @@ import { useFormStatus } from "react-dom";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
+import React from "react";
 
-import React from 'react'
-
-const FormSubmit = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface FormSubmitProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
 }
 
-export default FormSubmit
+const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant = "default", // Set a default value for variant
+}: FormSubmitProps) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      disabled={pending || disabled}
+      type="submit"
+      variant={variant}
+      size="sm"
+      className={cn(className)} // Optional: Use cn to handle class names conditionally
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default FormSubmit;
