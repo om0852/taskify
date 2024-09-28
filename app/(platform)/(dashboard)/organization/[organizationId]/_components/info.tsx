3ed457ea2dd2f72fa777736545d68b648c0961d3
om salunke <omsaunke343@gmail.com>
2024-09-28 23:34:39 +0530

    skeleton added for info card

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,23 +1,51 @@
-"use client"
-import { useOrganization } from '@clerk/nextjs'
-import Image from 'next/image';
-import React from 'react'
+"use client";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useOrganization } from "@clerk/nextjs";
+import { CreditCard } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Info = () => {
-  const {organization,isLoaded}=useOrganization();
-  if(!isLoaded){
-    return(
-      <p>Loading...</p>
-    )
+  const { organization, isLoaded } = useOrganization();
+  if (!isLoaded) {
+    return <Info.Skeleton />;
   }
   return (
-    <div className='flex items-center gap-x-4'>
+    <div className="flex items-center gap-x-4">
       <div className="w-[60px] h-[60px] relative">
-        <Image alt='Org' fill src={organization?.imageUrl!} className='rounded-md object-cover'/>
+        <Image
+          alt="Org"
+          fill
+          src={organization?.imageUrl!}
+          className="rounded-md object-cover"
+        />
       </div>
-      
+      <div className="space-y-1">
+        <p className="font-semibold text-xl">{organization?.name}</p>
+        <div className="flex items-center text-sm text-muted-foreground">
+          <CreditCard className="h-3 w-3 mr-1" />
+          Free
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
+
+Info.Skeleton = function SkeletonInfo() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Skeleton className="w-full h-full absolute" />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-10 w-[200px]" />
+        <div className="flex items-center">
+          <Skeleton className="h-4vw-4 mr-2" />
+          <Skeleton className="h-4 w-[100px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,14 @@
 import { db } from "@/lib/db";
 import React from "react";
 import Info from "./_components/info";
+import { Separator } from "@/components/ui/separator";
 
 const Page = async () => {
   const boards = await db.board.findMany();
   return (
     <div className="w-full mb-20">
       <Info/>
-    
+    <Separator className="my-4"/>
     </div>
   );
 };
