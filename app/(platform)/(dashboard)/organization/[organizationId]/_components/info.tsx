87c555a3dc86181aac648b168b22741b3eace3c4
om salunke <omsaunke343@gmail.com>
2024-09-28 23:25:43 +0530

    creating info card

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,9 +1,20 @@
 "use client"
+import { useOrganization } from '@clerk/nextjs'
+import Image from 'next/image';
 import React from 'react'
 
 const Info = () => {
+  const {organization,isLoaded}=useOrganization();
+  if(!isLoaded){
+    return(
+      <p>Loading...</p>
+    )
+  }
   return (
-    <div>
+    <div className='flex items-center gap-x-4'>
+      <div className="w-[60px] h-[60px] relative">
+        <Image alt='Org' fill src={organization?.imageUrl!} className='rounded-md object-cover'/>
+      </div>
       
     </div>
   )
