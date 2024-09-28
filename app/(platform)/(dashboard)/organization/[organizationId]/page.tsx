0f91f992c7e50846d5e2706ea60fe0be402e86de
om salunke <omsaunke343@gmail.com>
2024-09-28 23:42:17 +0530

    boardlisting component created

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,13 +2,17 @@ import { db } from "@/lib/db";
 import React from "react";
 import Info from "./_components/info";
 import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/BoardList";
 
 const Page = async () => {
   const boards = await db.board.findMany();
   return (
     <div className="w-full mb-20">
-      <Info/>
-    <Separator className="my-4"/>
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList/>
+      </div>
     </div>
   );
 };
