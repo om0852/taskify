5bd5f4a427b663f8181b56f9ecda05077a4ceef3
om salunke <omsaunke343@gmail.com>
2024-09-30 23:46:20 +0530

    create new board

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -24,6 +24,18 @@ const FormPopover = ({
   sideOffset = 0,
   align,
 }: FormPopoverProps) => {
+  const {execute,FieldErrors}=useAction(createBoard,{
+    onSuccess(data) {
+      console.log({data});
+    },
+    onError(error) {
+      console.log(error)
+    },
+  })
+  const onSumbit=(formData:FormData)=>{
+    const title = formData.get("title") as string;
+    execute({title});
+  }
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -37,6 +49,14 @@ const FormPopover = ({
           Create board
         </div>
         <PopoverClose asChild><Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600 " variant={"ghost"}><X className="h-4 w-4"/></Button></PopoverClose>
+        <form action={onSumbit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput id="title" label="Board title" type="text" errors={FieldErrors}/>
+          </div>
+          <FormSubmit className="w-full">
+            Create
+          </FormSubmit>
+        </form>
       </PopoverContent>
     </Popover>
   );
