390ecc79894e13c2b667b5f7cc3698de76ec38d6
om salunke <omsaunke343@gmail.com>
2024-10-03 00:51:52 +0530

    boardtitleform created

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        primary:"bg-sky-700 text-primary-foreground hover:bg-sky-700/30",
+        transparent:"bg-transparent text-white hover:bg-white/20"
       },
       size: {
         default: "h-10 px-4 py-2",
