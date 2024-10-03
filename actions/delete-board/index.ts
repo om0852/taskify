a6e5cf51eb814b7bda20343ccc78cec72e605dd2
om salunke <omsaunke343@gmail.com>
2024-10-04 00:33:33 +0530

    deleting board completed

---
+++ actions/delete-board/index.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { InputType, ReturnType } from "./types";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "../create-board/create-safe-action";
+import { DeleteBoard } from "./schema";
+import { redirect } from "next/navigation";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+  const { id } = data;
+  let board;
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+  } catch (e) {
+    return {
+      error: "Failed to delete",
+    };
+  }
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
--- app/(platform)/(dashboard)/board/_components/board-options.tsx
+++ app/(platform)/(dashboard)/board/_components/board-options.tsx
@@ -1,13 +1,26 @@
+"use client"
+import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
 import { PopoverTrigger, Popover, PopoverClose } from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
 import { PopoverContent } from "@radix-ui/react-popover";
 import { MoreHorizontal, X } from "lucide-react";
+import { toast } from "sonner";
 
 interface BoardOptionsProps {
   id: string;
 }
 
 export const BoardOptions = ({ id }: BoardOptionsProps) => {
+
+  const {execute,isLoading}=useAction(deleteBoard,{
+    onError(error) {
+     toast.error(error) 
+    }
+  });
+  const onDelete=()=>{
+    execute({id});
+  }
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -15,19 +28,19 @@ export const BoardOptions = ({ id }: BoardOptionsProps) => {
           <MoreHorizontal className="h-4 w-4 " />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
-        <div className="text-sm w-[30vh] font-medium text-center bg-white text-neutral-600 p-2">
+      <PopoverContent className="px-2 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm  font-medium text-center bg-white text-neutral-600 pb-4">
           Board Action
-            </div>
+        </div>
         <PopoverClose asChild>
           <Button
-            className="h-auto w-auto p-2 absolute top-4 right-2 text-neutral-600"
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant={"ghost"}
           >
             <X className="h-4 w-4" />
           </Button>
         </PopoverClose>
-        <Button className="">Delete this board</Button>
+        <Button disabled={isLoading} className="rounded-none w-full h-auto px-5p-2 justify-start font-normal text-sm" onClick={onDelete} variant={"ghost"}>Delete this board</Button>
       </PopoverContent>
     </Popover>
   );
