5a8292d50930d2d46ca7d3883b12ca99d1dd7d07
om salunke <omsaunke343@gmail.com>
2024-10-08 22:17:25 +0530

    updating description done

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -1,20 +1,25 @@
-import {z} from "zod";
-export const UpdateCard =z.object({
-    title:z.string({
-        required_error:"Title is required",
-        invalid_type_error:"TItle is required"
-    }).min(3,{
-        message:"Title is too short"
-    }),
-    id:z.string(),
-    boardId:z.string(),
-    description:z.optional(
-        z.string({
-            required_error:"Description is required",
-            invalid_type_error:"Description is required"
-            
-        }).min(3,{
-            message:"Description is too short.",
-        })
-    )
-})
+import { z } from "zod";
+export const UpdateCard = z.object({
+  title: z.optional(
+    z
+      .string({
+        required_error: "Title is required",
+        invalid_type_error: "TItle is required",
+      })
+      .min(3, {
+        message: "Title is too short",
+      })
+  ),
+  id: z.string(),
+  boardId: z.string(),
+  description: z.optional(
+    z
+      .string({
+        required_error: "Description is required",
+        invalid_type_error: "Description is required",
+      })
+      .min(3, {
+        message: "Description is too short.",
+      })
+  ),
+});
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,6 +5,7 @@ import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import Header from "./header";
+import Description from "./description";
 
 const CardModal = () => {
   const id = useCardModel((state) => state.id);
@@ -16,13 +17,21 @@ const CardModal = () => {
   });
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      {!cardData ? (
-        <Header.Skeleton />
-      ) : (
-        <DialogContent>
-          <Header data={cardData} />
-        </DialogContent>
-      )}
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
